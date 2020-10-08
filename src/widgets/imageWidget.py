66ead69617f8003e0aca51080e204a5d910ff44e
PauloMesquita <p.mesquita@vp6.com.br>
2020-10-07 23:54:55 -0300

    add zoom to image

--- src/widgets/imageWidget.py
+++ src/widgets/imageWidget.py
@@ -3,11 +3,23 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QLabel
 
 
+
 class ImageWidget(QtWidgets.QWidget):
     def __init__(self, imagePath, MainWindow):
         super(ImageWidget, self).__init__()
         print(imagePath)
-        pixmap = QPixmap(imagePath)
+        self.originalPixmap = QPixmap(imagePath)
+        self.pixmap = self.originalPixmap
         self.label = QLabel(self)
-        self.label.setPixmap(pixmap)
-        MainWindow.resize(pixmap.size())
+        self.label.setPixmap(self.pixmap)
+        self.scale = 1
+        # MainWindow.resize(self.pixmap.size())
+
+
+    def wheelEvent(self, event):
+        side = int(event.angleDelta().y()/120)
+        if (self.scale < 2 and side > 0) or (self.scale > 0.5 and side < 0):
+            self.scale += side/10
+        self.pixmap = self.originalPixmap.scaled(self.originalPixmap.width()*self.scale, self.originalPixmap.height()*self.scale)
+        self.label.setPixmap(self.pixmap)
+        self.label.adjustSize()
