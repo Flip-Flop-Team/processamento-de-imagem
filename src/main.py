27b9c0073dc7ae2ce960cb0e580b346b3d1903c6
PauloMesquita <p.mesquita@vp6.com.br>
2020-10-18 15:17:34 -0300

    marcar area com quadrado verde, mover a imagem em uma scroll area e mudar o sistema de zoom

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 from PyQt5.QtCore import QCoreApplication, Qt, QT_TR_NOOP as tr
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QFileDialog, QAction
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QFileDialog, QAction, QMessageBox
 from widgets.imageWidget import ImageWidget
 import signal
 
@@ -37,9 +37,53 @@ class MainWindow(QMainWindow):
 
     def topMenu(self):
         topMenu = self.menuBar()
+
         fileMenu = topMenu.addMenu('&File')
+        actionMenu = topMenu.addMenu('&Actions')
+
         fileMenu.addAction(self.createTopMenuAction(
             '&Open Image', 'Ctrl+O', 'Open a image', self.openImage))
+        actionMenu.addAction(self.createTopMenuAction('&Select region', 'Ctrl+S', 'Select a region', self.selectRegion))
+        actionMenu.addAction(self.createTopMenuAction('Z&oom', 'Ctrl+=', 'Zoom a image', self.zoomImage))
+        actionMenu.addAction(self.createTopMenuAction('U&nzoom', 'Ctrl+-', 'Unzoom a image', self.unzoomImage))
+        actionMenu.addAction(self.createTopMenuAction('Unselect all actions', 'Esc', 'Unselect all actions', self.unselect))
+
+    def unselect(self):
+        self.currentWidget.drawReact = False
+        self.currentWidget.label.setPixmap(self.currentWidget.zoomedPixmap)
+
+    def zoomImage(self):
+        if hasattr(self, 'currentWidget'):
+            size = self.currentWidget.originalPixmap.size()
+            self.currentWidget.zoomFunction(1)
+        else:
+            self.showErrorMessage('You need to open a image first')
+
+    def unzoomImage(self):
+        if hasattr(self, 'currentWidget'):
+            size = self.currentWidget.originalPixmap.size()
+            self.currentWidget.zoomFunction(-1)
+        else:
+            self.showErrorMessage('You need to open a image first')
+
+    def selectRegion(self):
+        if hasattr(self, 'currentWidget'):
+            size = self.currentWidget.originalPixmap.size()
+            if size.width() < 128 or size.height() < 128:
+                self.showErrorMessage('Image needs to be at least 128x128 pixels')
+            else:
+                self.currentWidget.drawReact = True
+        else:
+            self.showErrorMessage('You need to open a image first')
+            
+
+    def showErrorMessage(self, text):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText("Error")
+        msg.setInformativeText(text)
+        msg.setWindowTitle("Error")
+        msg.exec_()
 
     def createTopMenuAction(self, text, shortcut, statusTip, func):
         action = QAction(text, self)
@@ -51,6 +95,8 @@ class MainWindow(QMainWindow):
     def openImage(self):
         imagePath, _ = QFileDialog.getOpenFileName(
             self, 'Open Image', '', '*.png *.TIFF *.DICOM')
+        if imagePath == "":
+            return
         imageBackground = ImageWidget(imagePath, self)
         self.currentWidget = imageBackground
         self.setCentralWidget(self.currentWidget)
--- src/widgets/imageWidget.py
+++ src/widgets/imageWidget.py
@@ -1,23 +1,72 @@
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QPainter, QBrush, QColor
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QLabel, QScrollArea
+from PyQt5 import QtCore
+import copy
 
 
 class ImageWidget(QtWidgets.QWidget):
     def __init__(self, imagePath, MainWindow):
         super(ImageWidget, self).__init__()
-        print(imagePath)
+        mainWindowSize = copy.deepcopy(MainWindow.size())
+        mainWindowSize.setHeight(mainWindowSize.height()-30)
+        self.resize(mainWindowSize)
         self.originalPixmap = QPixmap(imagePath)
-        self.pixmap = self.originalPixmap
-        self.label = QLabel(self)
-        self.label.setPixmap(self.pixmap)
+        self.zoomedPixmap = self.originalPixmap
+        self.scrollArea = QScrollArea(self)
+        self.scrollArea.resize(self.size())
+        self.scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+        self.label = QLabel()
+        self.label.setPixmap(self.originalPixmap)
+        self.label.resize(self.originalPixmap.size())
+        self.scrollArea.setWidget(self.label)
         self.scale = 1
 
-    def wheelEvent(self, event):
-        side = int(event.angleDelta().y()/120)
-        if (self.scale < 5 and side > 0) or (self.scale > 0.5 and side < 0):
-            self.scale += side/10
-        self.pixmap = self.originalPixmap.scaled(
+        #Modes
+        self.drawReact = False
+        self.zoom = False
+        
+        self.mousePosition = QtCore.QPoint()
+        self.label.setMouseTracking(True)
+        self.scrollArea.setMouseTracking(True)
+        self.setMouseTracking(True)
+
+    def mouseMoveEvent(self, event):
+        self.mousePosition = event.pos()
+        self.update()
+
+    def mousePressEvent(self, e):
+        self.drawReact = False
+
+    def zoomFunction(self, direction):
+        if (self.scale < 5 and direction > 0) or (self.scale > 0.5 and direction < 0):
+            self.scale += direction/10
+        self.zoomedPixmap = self.originalPixmap.scaled(
             self.originalPixmap.width()*self.scale, self.originalPixmap.height()*self.scale)
-        self.label.setPixmap(self.pixmap)
-        self.label.adjustSize()
+        self.label.setPixmap(self.zoomedPixmap)
+        self.label.resize(self.zoomedPixmap.size())
+
+    def paintEvent(self, e):
+        if self.drawReact:
+            x = self.mousePosition.x()+self.scrollArea.horizontalScrollBar().value()
+            y = self.mousePosition.y()+self.scrollArea.verticalScrollBar().value()
+            pixmapX = self.zoomedPixmap.size().width()
+            pixmapY = self.zoomedPixmap.size().height()
+
+            if x < 64: x = 64
+            if y < 64: y = 64
+            if x > pixmapX-64: x = pixmapX-64
+            if y > pixmapY-64: y = pixmapY-64
+
+            self.label.setPixmap(self.zoomedPixmap)
+            qp = QPainter(self.label.pixmap())
+            br = QBrush(QColor(0, 255, 0, 40))
+            qp.setBrush(br)
+            firstPoint = copy.deepcopy(self.mousePosition)
+            secondPoint = copy.deepcopy(self.mousePosition)
+            firstPoint.setX(x-64)
+            firstPoint.setY(y-64)
+            secondPoint.setX(x+64)
+            secondPoint.setY(y+64)
+            qp.drawRect(QtCore.QRect(firstPoint, secondPoint))
